cf60c60ac0ddad977e99978cbf90f859763c0bc7
luqmanCodes <37939871+luqmanCodes@users.noreply.github.com>
2018-09-26 13:53:46 +0530

    initial commit with application properties

---
+++ SpringBackendAPI/src/main/java/lk/sliit/csse/group19/springApi/SpringBackendAPI/SpringBackendApiApplication.java
@@ -0,0 +1,12 @@
+package lk.sliit.csse.group19.springApi.SpringBackendAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBackendApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBackendApiApplication.class, args);
+	}
+}
---
+++ SpringBackendAPI/src/test/java/lk/sliit/csse/group19/springApi/SpringBackendAPI/SpringBackendApiApplicationTests.java
@@ -0,0 +1,16 @@
+package lk.sliit.csse.group19.springApi.SpringBackendAPI;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBackendApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
